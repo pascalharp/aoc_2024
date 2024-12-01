0b6fca34a424e6148502e4cc891e6a2785e2dca5
Pascal Harprecht <p.harprecht@gmail.com>
2024-12-01 13:31:25 +0100

    cargo fmt

--- src/day/day01.rs
+++ src/day/day01.rs
@@ -7,32 +7,33 @@ use super::Day;
 pub struct Day01;
 
 fn to_vecs(input: &str) -> anyhow::Result<(Vec<i64>, Vec<i64>)> {
-        let line_count = input.lines().count();
-        let mut left: Vec<i64> = Vec::with_capacity(line_count);
-        let mut right: Vec<i64> = Vec::with_capacity(line_count);
+    let line_count = input.lines().count();
+    let mut left: Vec<i64> = Vec::with_capacity(line_count);
+    let mut right: Vec<i64> = Vec::with_capacity(line_count);
 
-        for l in input.lines() {
-            let mut parts = l.split_whitespace();
-            left.push(parts.next().context("Expected left number")?.parse()?);
-            right.push(parts.next().context("Expected right number")?.parse()?);
-        }
+    for l in input.lines() {
+        let mut parts = l.split_whitespace();
+        left.push(parts.next().context("Expected left number")?.parse()?);
+        right.push(parts.next().context("Expected right number")?.parse()?);
+    }
 
-        Ok((left, right))
+    Ok((left, right))
 }
 
 impl Day for Day01 {
     fn one(&self, input: &str) -> anyhow::Result<i64> {
-
         let (mut left, mut right) = to_vecs(input)?;
 
         left.sort();
         right.sort();
 
-        Ok(left.into_iter().zip(right).map(|(left, right)| (right - left).abs()).sum())
-
+        Ok(left
+            .into_iter()
+            .zip(right)
+            .map(|(left, right)| (right - left).abs())
+            .sum())
     }
     fn two(&self, input: &str) -> anyhow::Result<i64> {
-
         let (left, right) = to_vecs(input)?;
         let mut counts: HashMap<i64, i64> = HashMap::new();
 
@@ -40,7 +41,10 @@ impl Day for Day01 {
             counts.entry(i).and_modify(|e| *e += 1).or_insert(1);
         }
 
-        Ok(left.into_iter().map(|i| i * counts.get(&i).unwrap_or(&0)).sum())
+        Ok(left
+            .into_iter()
+            .map(|i| i * counts.get(&i).unwrap_or(&0))
+            .sum())
     }
 }
 
@@ -60,7 +64,7 @@ mod tests {
         let result = d.one(&INPUT).unwrap();
         assert_eq!(result, 11);
     }
-    
+
     #[test]
     fn two() {
         let d = Day01;
